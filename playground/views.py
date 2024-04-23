e09b049d5114336d68f7b303a4ed8d2c63effdb8
CJ21 <jconuekwusi@gmail.com>
2024-04-23 07:46:29 +0100

    end of Django ORM

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Value, F, ExpressionWrapper, DecimalField
+from store.models import Product, Collection, Customer, Order, OrderItem
+from tags.models import TaggedItem
+from django.contrib.contenttypes.models import ContentType
+from django.db import connection
 
+def say_hello(request): 
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers', [1,2,'a'])
 
-def calculate():
-    x = 1
-    y = 2
-    return x
-
-
-def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})
+  
